fca394b29bac04a25f79b4ce58f5248d478a599d
Pranit Karkera <pranitkarkera99@gmail.com>
2025-02-04 14:46:43 +0530

    rectified user schema

--- models/users.models.js
+++ models/users.models.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
