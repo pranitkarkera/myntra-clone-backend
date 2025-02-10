324cfbf861ea7d506350b0f2ee9e798f06350dbd
Pranit Karkera <pranitkarkera99@gmail.com>
2025-02-10 23:30:05 +0530

    input validation

--- models/address.models.js
+++ models/address.models.js
@@ -2,24 +2,24 @@ const mongoose = require("mongoose");
 
 const addressSchema = new mongoose.Schema(
   {
-    // userId: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "User",
-    //   required: true,
-    // },
-    name: { type: String, required: true },
-    number: { type: Number, required: true },
-    street: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    zipCode: { type: String, required: true },
-    country: { type: String, required: true },
+    name: { type: String, required: true, trim: true, minlength: 3 },
+    number: { type: String, required: true, trim: true },
+    street: { type: String, required: true, trim: true },
+    city: { type: String, required: true, trim: true },
+    state: { type: String, required: true, trim: true },
+    zipCode: {
+      type: String,
+      required: true,
+      match: [/^\d{5,6}$/, "Invalid Zip Code"],
+    },
+    country: { type: String, required: true, trim: true },
     addressType: {
       type: String,
       required: true,
       enum: ["Home", "Work"],
+      trim: true,
     },
-    defaultAddress: {type: Boolean}
+    defaultAddress: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
--- models/users.models.js
+++ models/users.models.js
@@ -1,13 +1,29 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    username: { type: String, unique: true, required: true },
+    name: { type: String, required: true, trim: true, minlength: 3 },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Invalid email address"],
+    },
+    password: { type: String, required: true, minlength: 8 },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      minlength: 3,
+      trim: true,
+      lowercase: true,
+    },
   },
-  { timestamps: true }
+  { timestamps: true, strict: "throw" }
 );
+
 
 module.exports = mongoose.model("User", userSchema);
