b283c1d7b503c9a1a8bdc49e85d820c0b35f7d80
Pranit Karkera <pranitkarkera99@gmail.com>
2025-02-14 17:53:49 +0530

    .

--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const categoryController = require("../controllers/categoriesController");
-const authMiddleware = require("../auth/authMiddlerware"); // Import the middleware
 
 const router = express.Router();
 
 // Create a new category (protected route)
-router.post("/", authMiddleware, categoryController.createCategory);
+router.post("/", categoryController.createCategory);
 
 // Get all categories (protected route)
-router.get("/", authMiddleware, categoryController.getCategories);
+router.get("/", categoryController.getCategories);
 
 // Get category by ID (protected route)
-router.get("/:categoryId", authMiddleware, categoryController.getByCategoryId);
+router.get("/:categoryId", categoryController.getByCategoryId);
 
 module.exports = router;
