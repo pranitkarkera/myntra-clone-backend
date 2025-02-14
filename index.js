a15e17d85df1b351fd553a95d5bc8aeff053cabd
Pranit Karkera <pranitkarkera99@gmail.com>
2025-02-14 16:30:42 +0530

    .

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-// const session = require("express-session");
 const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const productRoutes = require("./routes/productsRoutes");
 const categoryRoutes = require("./routes/categoriesRoutes");
 const wishlistRoutes = require("./routes/wishlistRoutes");
@@ -12,31 +13,30 @@ const { initializeDatabase } = require("./db/db.connection");
 
 const app = express();
 
+// CORS configuration
 const corsOptions = {
-  origin: "*",
+  origin: ["http://localhost:3000", "https://myntra-puce-ten.vercel.app"], // Specify allowed origins
   credentials: true,
-  optionSuccessStatus: 200,
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
+app.use(helmet()); // Use Helmet for security
 app.use(express.json());
 
-// const secretKey = process.env.Your_Secrey_Key
-
-// // Session middleware
-// app.use(session({
-//   secret: secretKey,
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: true }
-// }));
-
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
 
 // Test route
 app.get("/", (req, res) => {
-  res.json({ message: "Api is working!" });
+  res.json({ message: "API is working!" });
 });
 
+// Route definitions
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/wishlist", wishlistRoutes);
@@ -44,10 +44,20 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/address", addressRoutes);
 app.use("/api/user", userRoutes);
 
-
-initializeDatabase();
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
 });
+
+// Initialize database
+initializeDatabase()
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed:", error);
+  });
