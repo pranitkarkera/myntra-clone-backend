65c0ecf313167b41b5d27a5febbd1275403c00cb
Pranit Karkera <pranitkarkera99@gmail.com>
2025-03-24 01:10:58 +0530

    .

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,5 +12,8 @@ router.post("/login", loginValidation, userController.login);
 router.get("/:userId", ensureAuthenticated, userController.getUser);
 router.put("/:userId", ensureAuthenticated, userController.updateUser);
 router.delete("/:userId", ensureAuthenticated, userController.deleteUser);
-
+// Validate token route (for RefreshHandler)
+router.post("/validate-token", ensureAuthenticated, (req, res) => {
+  res.status(200).json({ message: "Token is valid" });
+});
 module.exports = router;
